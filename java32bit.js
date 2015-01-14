c4414d03c522ce32797d13da63763cef3ab4c47b
Oleksandr Knyga <oleksandrknyga@gmail.com>
2015-01-14 04:13:16 -0800

    sha1

--- java32bit.js
+++ java32bit.js
@@ -1,9 +1,9 @@
 define(function(require) {
-    var hashCode = function(){
+    var hashCode = function(data){
         var hash = 0;
-        if (this.length == 0) return hash;
-        for (i = 0; i < this.length; i++) {
-            char = this.charCodeAt(i);
+        if (data.length == 0) return hash;
+        for (i = 0; i < data.length; i++) {
+            char = data.charCodeAt(i);
             hash = ((hash<<5)-hash)+char;
             hash = hash & hash; // Convert to 32bit integer
         }
